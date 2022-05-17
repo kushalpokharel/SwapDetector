f4f5eb24ef1770fb5cfc8ba6aa9801898e0aa659
kushalpokharel <kusaalpokharel@gmail.com>
2022-05-17 16:31:25 +0545

    transactions filtered based on swap event

--- src/agent.ts
+++ src/agent.ts
@@ -18,11 +18,11 @@ const factoryabi = require('/home/kushal/Work/uniswap_swap_detector/src/factorya
 
 let findingsCount = 0;
 const provider = new Web3.providers.HttpProvider(
-  'https://rinkeby.infura.io/v3/11b5da3171f94a138ac566452555eba7'
+  'https://mainnet.infura.io/v3/a0f979b01d5f4fbaae2b84575d7fb601'
 );
 let web3 = new Web3(provider);
 
-const pairAddresses: string[] = [];
+// const pairAddresses: string[] = [];
 
 async function getAddr(token0:string,token1:string) {
   const uniContract = new web3.eth.Contract(
@@ -45,39 +45,31 @@ const initialize: Initialize = async() =>{
 
   const token0 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase(); //dai token
   const token1 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase(); //weth token
-  const factory = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
-  const _hexadem = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
 
-  const condition = web3.utils.soliditySha3(token0,token1,3000);
-  let caddress = "";
-  if(condition)
-    caddress = getCreate2Address(factory,condition,_hexadem)
-
-  const ad = await getAddr(token0,token1);
+  // const ad = await getAddr(token0,token1);
   const create2addr = v3Create2(token0, token1, 3000);
 
   console.log(create2addr);
-  console.log("address "+ad);
-  pairAddresses.push(ad);
+  // console.log("address "+ad);
+  // pairAddresses.push(ad);
 }
 
-export const FUNCTION_NAME = "function swap(address recipient, bool zeroForOne, int256 amountSpecified, uint160 sqrtPriceLimitX96, bytes data)";
+export const EVENT_NAME = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
-
+  // console.log(txEvent);
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
-  
   // filter the transaction logs for Tether transfer events
-  const swapEvents = txEvent.filterFunction(
-    [FUNCTION_NAME],
-    pairAddresses[0]
+  const swapEvents = txEvent.filterLog(
+    EVENT_NAME
   );
 
   swapEvents.forEach((swapEvent) => {
+    console.log("here");
     // extract transfer event arguments
     const { to, from, value } = swapEvent.args;
     // shift decimals of transfer value
@@ -109,7 +101,6 @@ const handleTransaction: HandleTransaction = async (
 // }
 
 export default {
-  initialize,
   handleTransaction,
   // handleBlock
 };
