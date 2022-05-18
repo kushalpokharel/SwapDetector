d3a76eff03a3177b37a5076c08169c7e2b5f6140
kushalpokharel <kusaalpokharel@gmail.com>
2022-05-18 13:53:02 +0545

    working with getPool but not with create2

--- src/agent.ts
+++ src/agent.ts
@@ -6,10 +6,10 @@ import {
   TransactionEvent,
   FindingSeverity,
   FindingType,
-  Initialize
+  Initialize,
+  getJsonRpcUrl
 } from "forta-agent";
 
-import  Web3 from "web3";
 import {BigNumberish, ethers} from "ethers";
 import { getCreate2Address } from "ethers/lib/utils";
 import { defaultAbiCoder } from "ethers/lib/utils";
@@ -17,42 +17,55 @@ import { keccak256 } from "ethers/lib/utils";
 const factoryabi = require('/home/kushal/Work/uniswap_swap_detector/src/factoryabi.json');
 
 let findingsCount = 0;
-const provider = new Web3.providers.HttpProvider(
-  'https://mainnet.infura.io/v3/a0f979b01d5f4fbaae2b84575d7fb601'
-);
-let web3 = new Web3(provider);
-
-// const pairAddresses: string[] = [];
-
-async function getAddr(token0:string,token1:string) {
-  const uniContract = new web3.eth.Contract(
-    factoryabi,
-    '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
+const poolAbi  =  [
+  "function token0() public view returns (address)",
+  "function token1() public view returns (address)",
+  "function fee() public view returns (uint24)"
+]
+const getReference = (addr:string) =>{
+  const contractRef = new ethers.Contract(
+    addr,
+    poolAbi,
+    provider
   );
-  let a =  await uniContract.methods.getPool(token0,token1,3000).call();
+  return contractRef;
+}
+
+const uniContract = new ethers.Contract(
+  '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+  factoryabi,
+  provider
+);
+
+//working correctly: querying the factory contract and returning the pool address
+async function getAddr(token0:string,token1:string, fee:BigNumberish) {
+  
+  let a =  await uniContract.getPool(token0,token1,fee);
   console.log("addr " +a);
   return a;
 }
 
+//not working correctly: calculating the address using create2.
 const v3Create2 = (token0:string, token1:string, fee:BigNumberish) => {
   const salt: string = keccak256(defaultAbiCoder.encode(["address", "address", "uint24"], [token0, token1, fee]));
-  const V3_FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984'.toLowerCase();
+  const V3_FACTORY = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
   const V3_PAIR_INIT_CODE = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f';
   return getCreate2Address(V3_FACTORY, salt, V3_PAIR_INIT_CODE).toLowerCase();
 }
 
-const initialize: Initialize = async() =>{
+// const initialize: Initialize = async() =>{
 
-  const token0 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase(); //dai token
-  const token1 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase(); //weth token
+//   const token0 = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'.toLowerCase(); //dai token
+//   const token1 = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase(); //weth token
 
-  // const ad = await getAddr(token0,token1);
-  const create2addr = v3Create2(token0, token1, 3000);
+//   // const ad = await getAddr(token0,token1);
+//   const create2addr = v3Create2(token0, token1, 3000);
 
-  console.log(create2addr);
-  // console.log("address "+ad);
-  // pairAddresses.push(ad);
-}
+//   console.log(create2addr);
+//   // console.log("address "+ad);
+//   // pairAddresses.push(ad);
+// }
 
 export const EVENT_NAME = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
 
@@ -63,33 +76,50 @@ const handleTransaction: HandleTransaction = async (
   // console.log(txEvent);
   // limiting this agent to emit only 5 findings so that the alert feed is not spammed
   if (findingsCount >= 5) return findings;
-  // filter the transaction logs for Tether transfer events
+  // filter the transaction logs for Swap events
   const swapEvents = txEvent.filterLog(
     EVENT_NAME
   );
-
-  swapEvents.forEach((swapEvent) => {
-    console.log("here");
-    // extract transfer event arguments
-    const { to, from, value } = swapEvent.args;
-    // shift decimals of transfer value
-    
-
-    findings.push(
-      Finding.fromObject({
-        name: "SWAP",
-        description: `Swap method called`,
-        alertId: "FORTA-1",
-        severity: FindingSeverity.Low,
-        type: FindingType.Info,
-        metadata: {
-          to,
-          from,
-        },
-      })
-    );
-    findingsCount++;
-  });
+  await Promise.all(
+    swapEvents.map(async (swapEvent)  => {
+      // console.log(swapEvent);
+      // extract the pool address from the swap event
+      const contractAddress = swapEvent.address;
+      //get the smart contract reference from the above address
+      const contractRef = getReference(contractAddress);
+      //get these parameters from the pool contract above
+      const token0Addr = await contractRef.token0();
+      const token1Addr = await contractRef.token1();
+      const fee = await contractRef.fee();
+      //get/calculate the pool contract from above parameters
+      const calcAddr = v3Create2(token0Addr,token1Addr,fee);
+      const calcAddr1 = await getAddr(token0Addr,token1Addr,fee);
+      //check if the pool address is same 
+      if(calcAddr1.toLowerCase()===contractAddress.toLowerCase()){
+        findings.push(
+          Finding.fromObject({
+            name: "SWAP",
+            description: `Swap method called in the pool ${calcAddr1}`,
+            alertId: "FORTA-1",
+            severity: FindingSeverity.Low,
+            type: FindingType.Info,
+            metadata: {
+              contractAddress,
+              token0Addr,
+              token1Addr,
+              fee
+            },
+          })
+        );
+      }
+      // console.log("calcAddr " +calcAddr);
+      // console.log("calcAddr1 " +calcAddr1);
+      // console.log("token1 "+token1Addr);
+      // console.log(contractAddress);
+      
+      findingsCount++;
+    })
+  );
 
   return findings;
 };
